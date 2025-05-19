b1d9ab31245e863bb5c84f23be2f00fb09ef9350
guilhermesptech <guilherme.doliveira@sptech.school>
2025-05-19 01:28:27 -0300

    feat: recebendo os dados do client, validando e passando para a model

--- src/controllers/usuarioController.js
+++ src/controllers/usuarioController.js
@@ -52,25 +52,28 @@ function autenticar(req, res) {
 }
 
 function cadastrar(req, res) {
+
     // Crie uma variável que vá recuperar os valores do arquivo cadastro.html
-    var nome = req.body.nomeServer;
-    var email = req.body.emailServer;
-    var senha = req.body.senhaServer;
-    var fkEmpresa = req.body.idEmpresaVincularServer;
+    var nome = req.body.nome;
+    var sobrenome = req.body.sobrenome;
+    var email = req.body.email;
+    var senha = req.body.senha;
+    var codigoEmpresa = req.body.codigoEmpresa;
 
     // Faça as validações dos valores
     if (nome == undefined) {
         res.status(400).send("Seu nome está undefined!");
+    } else if (sobrenome == undefined) {
+        res.status(400).send("Seu sobrenome está undefined!");
     } else if (email == undefined) {
         res.status(400).send("Seu email está undefined!");
     } else if (senha == undefined) {
         res.status(400).send("Sua senha está undefined!");
-    } else if (fkEmpresa == undefined) {
-        res.status(400).send("Sua empresa a vincular está undefined!");
+    } else if (codigoEmpresa == undefined) {
+        res.status(400).send("O código da empresa está undefined!");
     } else {
-
         // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
-        usuarioModel.cadastrar(nome, email, senha, fkEmpresa)
+        usuarioModel.cadastrar(nome, sobrenome, email, senha, codigoEmpresa)
             .then(
                 function (resultado) {
                     res.json(resultado);
