449796bc4d3cdabfc17f3e24f55bca8938732281
guilhermesptech <guilherme.doliveira@sptech.school>
2025-05-19 01:30:34 -0300

    feat: fazendo a consulta com o código da empresa para retornar o id, verificando se o resultado é valido e persistindo o usuário no banco de dados

--- src/models/usuarioModel.js
+++ src/models/usuarioModel.js
@@ -10,16 +10,27 @@ function autenticar(email, senha) {
 }
 
 // Coloque os mesmos parâmetros aqui. Vá para a var instrucaoSql
-function cadastrar(nome, email, senha, fkEmpresa) {
-    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", nome, email, senha, fkEmpresa);
-    
+function cadastrar(nome, sobrenome, email, senha, codigoEmpresa) {
+    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", nome, sobrenome, email, senha, codigoEmpresa);
+
     // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
     //  e na ordem de inserção dos dados.
-    var instrucaoSql = `
-        INSERT INTO usuario (nome, email, senha, fk_empresa) VALUES ('${nome}', '${email}', '${senha}', '${fkEmpresa}');
+
+    const pegaId = `SELECT id FROM empresa WHERE codigo_empresa = '${codigoEmpresa}';`;
+
+    return database.executar(pegaId).then(resultado => {
+        if (resultado.length === 0) {
+            throw new Error("Código da empresa inválido ou não encontrado.");
+        }
+
+        const fkEmpresa = resultado[0].id;
+
+        var instrucaoSql = `
+        INSERT INTO usuario (nome, sobrenome, email, senha, data_cadastro, fkempresa) VALUES ('${nome}', '${sobrenome}', '${email}', '${senha}', NOW(), ${fkEmpresa});
     `;
-    console.log("Executando a instrução SQL: \n" + instrucaoSql);
-    return database.executar(instrucaoSql);
+        console.log("Executando a instrução SQL: \n" + instrucaoSql);
+        return database.executar(instrucaoSql);
+    });
 }
 
 module.exports = {
