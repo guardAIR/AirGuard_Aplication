99cd916074077c9552d40b008c36e96679326267
Bruno Oliveira <brunosilvadeoliveira.10@gmail.com>
2025-06-09 20:47:27 -0300

    adicionando campo de 'isadm' no bd e corrigindo links

--- src/models/usuarioModel.js
+++ src/models/usuarioModel.js
@@ -3,7 +3,7 @@ var database = require("../database/config")
 function autenticar(email, senha) {
     console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function entrar(): ", email, senha)
     var instrucaoSql = `
-        SELECT NomeUsuario, SobrenomeUsuario, EmailUsuario, dtCadastro, NomeEmpresa, idEmpresa, id FROM vwUsuario WHERE EmailUsuario = '${email}' AND SenhaUsuario = '${senha}';
+        SELECT NomeUsuario, SobrenomeUsuario, EmailUsuario, dtCadastro, NomeEmpresa, idEmpresa, id, isADM FROM vwUsuario WHERE EmailUsuario = '${email}' AND SenhaUsuario = '${senha}';
     `;
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
